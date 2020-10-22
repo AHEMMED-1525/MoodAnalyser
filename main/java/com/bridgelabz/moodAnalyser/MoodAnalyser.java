9701869d4bdd5609cf8eb47f7de8da8e3dfb94ac
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-22 09:14:53 +0530

    Added : Mood Analyser With Exception Handling using Try Catch block

--- main/java/com/bridgelabz/moodAnalyser/MoodAnalyser.java
+++ main/java/com/bridgelabz/moodAnalyser/MoodAnalyser.java
@@ -4,11 +4,13 @@ package com.bridgelabz.moodAnalyser;
 public class MoodAnalyser {
 
 	public String analyseMood(String message)  {
-
+		try {
 		if(message.contains("sad"))
 			return "sad";
-		
-
+		}
+		catch(NullPointerException e) {
+			return "happy";
+		}
 		return "happy";
 
 	}
--- test/java/com/bridgelabz/moodAnalyser/MoodAnalyserTest.java
+++ test/java/com/bridgelabz/moodAnalyser/MoodAnalyserTest.java
@@ -5,17 +5,22 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 	@Test
-	public void ReturnHappy() {
+	public void givenMood_WhenProper_ShouldReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
 		String mood = moodAnalyser.analyseMood("I am Happy");
 		Assert.assertEquals("happy", mood);
 	}
 
 	@Test
-	public void ReturnSad() {
+	public void givenMood_WhenProper_ShouldReturnSad() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
 		String mood = moodAnalyser.analyseMood("I am sad");
 		Assert.assertEquals("sad", mood);
 	}
-
+	@Test
+	public void givenMood_WhenProper_ShouldReturnHappyMood() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood(null);
+		Assert.assertEquals("happy", mood);
+	}
 }
